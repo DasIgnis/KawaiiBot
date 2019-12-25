6b3f0896c241304ca04c8edd5d48974e7ccd2581
DasIgnis <jana.zadvernyak@gmail.com>
2019-12-25 21:11:30 +0300

    aiml usage

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AIMLbot;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -23,12 +24,21 @@ namespace KawaiiBot
     {
 
         BackgroundWorker asyncWorker;
+        AIMLbot.Bot bot;
+        User user;
         public MainWindow()
         {
             InitializeComponent();
 
             asyncWorker = new BackgroundWorker();
             asyncWorker.DoWork += launchWorkerAsync;
+
+            bot = new AIMLbot.Bot();
+            bot.loadSettings();
+            bot.isAcceptingUserInput = false;
+            bot.loadAIMLFromFiles();
+            bot.isAcceptingUserInput = true;
+            user = new User("Уважаемый", bot);
         }
 
         async void launchWorkerAsync(object sender, DoWorkEventArgs e)
@@ -50,15 +60,19 @@ namespace KawaiiBot
                         var msgText = message.Message;
                         if (msgText.Type == Telegram.Bot.Types.Enums.MessageType.Text)
                         {
-                            if (msgText.Text == "/saysomething")
-                            {
-                                await Bot.SendTextMessageAsync(msgText.Chat.Id, "Ня!", replyToMessageId: msgText.MessageId);
-                            }
-                            if (msgText.Text.ToUpper() == "YARE YARE")
-                            {
-                                await Bot.SendPhotoAsync(msgText.Chat.Id, "https://i.pinimg.com/originals/bd/6e/5a/bd6e5a259c33a226550960b91ff857c4.jpg");
-                                await Bot.SendTextMessageAsync(msgText.Chat.Id, "Daze, красавчик ;^)");
-                            }
+                            //if (msgText.Text == "/saysomething")
+                            //{
+                            //    await Bot.SendTextMessageAsync(msgText.Chat.Id, "Ня!", replyToMessageId: msgText.MessageId);
+                            //}
+                            //if (msgText.Text.ToUpper() == "YARE YARE")
+                            //{
+                            //    await Bot.SendPhotoAsync(msgText.Chat.Id, "https://i.pinimg.com/originals/bd/6e/5a/bd6e5a259c33a226550960b91ff857c4.jpg");
+                            //    await Bot.SendTextMessageAsync(msgText.Chat.Id, "Daze, красавчик ;^)");
+                            //}
+                            Request r = new Request(msgText.Text.ToUpper(), user, bot);
+                            Result res = bot.Chat(r);
+                            if (res.Output != "")
+                                await Bot.SendTextMessageAsync(msgText.Chat.Id, res.Output);
                         }
                         offset = message.Id + 1;
                     }
@@ -67,6 +81,7 @@ namespace KawaiiBot
             } catch (Telegram.Bot.Exceptions.ApiRequestException exception)
             {
                 Console.WriteLine(exception.Message);
+                asyncWorker.RunWorkerAsync("999037946:AAHbd0xIjp5l6iS0aGuVB-jIP2R4a99EUFo");
             }
         }
 
