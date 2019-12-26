20db15f057b5418a48758209bf3a50fa00bcc0e6
Anton Stupak <antoxa.com.com@mail.ru>
2019-12-27 00:01:07 +0300

    Network

---
+++ Config.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KawaiiBot
+{
+    public class Config
+    {
+        public static Config current;
+        public List<string> figures;
+        public int epoches = 20;
+        public int training_size = 700, test_size = 300;
+        public int width = 640;
+        public int height = 480;
+        public int camera_width = 640;
+        public int camera_height = 480;
+        public string net_sctructure = "400;500;20;2";
+        public double accuracy = 0.7;
+        public bool parallel = true;
+        public string figure;
+        public int snapshot_timer_cooldown = 5000;
+        public Config SaveAsJson(string filepath)
+        {
+            using (StreamWriter file = File.CreateText(filepath))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                //serialize object directly into file stream
+                serializer.Serialize(file, this);
+            }
+            return this;
+        }
+
+        public static Config LoadFromJson(string filepath)
+        {
+            using (StreamReader r = new StreamReader(filepath))
+            {
+                string json = r.ReadToEnd();
+                return JsonConvert.DeserializeObject<Config>(json);
+            }
+        }
+        
+        public int figure_id(string figure)
+        {
+            return figures.FindIndex(x => x == figure);
+        }
+    };
+}
---
+++ ImageProcessor.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KawaiiBot
+{
+    public class ImageProcessor
+    {
+        static public Dictionary<string, string[]> files = new Dictionary<string, string[]>();
+        public void fillFiles()
+        {
+            foreach(var fig in Config.current.figures)
+            {
+                files[fig] = Directory.GetFiles(screenshots_dir(fig), "*.png");
+            }
+        }
+        private Random rand = new Random();
+
+        public Sample GenerateFigure()
+        {
+            var type = rand.Next(Config.current.figures.Count);
+            return process_output(generate_figure(type), type);
+        }
+        public SamplesSet GenerateSet()
+        {
+            SamplesSet res = new SamplesSet();
+            for (int i = 0; i < Config.current.figures.Count; i++)
+            {
+                var sel = Config.current.figures[i];
+                int take = Math.Min(files[sel].Length, Config.current.training_size);
+                //foreach(var el in files[sel].OrderBy(x => rand.Next()).Take(take))
+                Parallel.ForEach(files[sel].OrderBy(x => rand.Next()).Take(take), el =>
+                {
+                    res.AddSample(process_output(fillImage(new Bitmap(el)), i));
+                });
+            }
+            res.samples = res.samples.OrderBy(x => rand.Next()).ToList();
+            return res;
+        }
+        public Sample GenerateFigure(Bitmap btmp, int type, out Bitmap proc)
+        {
+            proc = processBitmap(btmp);
+            return process_output(fillImage(proc), type);
+        }
+        public Sample process_output(bool[,] img, int type)
+        {
+            double[] input = new double[Config.current.width + Config.current.height];
+            for (int i = 0; i < Config.current.width + Config.current.height; i++)
+                input[i] = 0;
+
+            for (int i = 0; i < Config.current.width; i++)
+                for (int j = 0; j < Config.current.height; j++)
+                    if (img[i, j])
+                    {
+                        input[i] += 1;
+                        input[Config.current.width + j] += 1;
+                    }
+
+            return new Sample(input, Config.current.figures.Count, type);
+        }
+        public byte grayTransform(Color pixel)
+        {
+            return (byte)(0.2126 * pixel.R + 0.7152 * pixel.G + 0.0722 * pixel.B);
+        }
+        public Bitmap grayShaded(Bitmap raw_image)
+        {
+            Bitmap gray_shades = new Bitmap(raw_image.Width, raw_image.Height);
+            for (int i = 0; i < raw_image.Width; i++)
+            {
+                for (int j = 0; j < raw_image.Height; j++)
+                {
+                    System.Drawing.Color pixel = raw_image.GetPixel(i, j);
+                    byte sum = grayTransform(pixel);
+                    System.Drawing.Color transform = System.Drawing.Color.FromArgb(pixel.A, sum, sum, sum);
+                    gray_shades.SetPixel(i, j, transform);
+                }
+            }
+            return gray_shades;
+        }
+        public Bitmap processBitmap(Bitmap raw_image)
+        {
+            //ClearImage();
+            Bitmap prepared = grayShaded(raw_image);
+            for (int i = 0; i < Config.current.width; i++)
+                for (int j = 0; j < Config.current.height; j++)
+                {
+                    var p = prepared.GetPixel(i, j);
+                    prepared.SetPixel(i, j, p.R < 100 ? Color.Black : Color.White);
+                }
+            return prepared;
+        }
+        public string screenshots_dir(string figure)
+        {
+            var res = Path.Combine("..", "..", $"screenshots({Config.current.width}x{Config.current.height})", figure.ToLower());
+            bool exists = System.IO.Directory.Exists(res);
+
+            if (!exists)
+                System.IO.Directory.CreateDirectory(res);
+            return res;
+        }
+        public Bitmap saveClassSample(string figure, Bitmap raw, out string path)
+        {
+            path = Path.Combine(screenshots_dir(figure), $"{Guid.NewGuid()}.png");
+            var b = processBitmap(raw);
+            b.Save(path, ImageFormat.Png);
+            return b;
+        }
+        public bool[,] generate_figure(int type = -1)
+        {
+            if (type == -1 || type >= Config.current.figures.Count)
+                type = rand.Next(Config.current.figures.Count);
+            var selected_class = Config.current.figures[type];
+            var f = files[selected_class];
+            return fillImage(new Bitmap(f[rand.Next(f.Length)]));
+        }
+        public bool[,] fillImage(Bitmap btmp)
+        {
+            bool[,] img = new bool[Config.current.width, Config.current.height];
+            for (int i = 0; i < Config.current.width; i++)
+                for (int j = 0; j < Config.current.height; j++)
+                {
+                    var p = btmp.GetPixel(i, j);
+                    if (p.R == 0 && p.G == 0 && p.B == 0)
+                        img[i, j] = true;
+                    img[i, j] = p.R == 0 && p.G == 0 && p.B == 0 ? true : false;
+                }
+            return img;
+        }
+    }
+}
